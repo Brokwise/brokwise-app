28c512fe8073e3200453ea2e8f5dad1aedfeb7ef
PradeepKundekar0101 <pradeepkundekargithub@gmail.com>
2026-02-26 13:17:04 +0530

    refactor(layout): replace Google Tag Manager with Contentsquare script
    - Removed Google Tag Manager integration and its associated scripts.
    - Added Contentsquare tracking script for enhanced analytics.
    - Cleaned up layout by removing the Toaster component and noscript fallback.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,30 +36,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
-        <Toaster />
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WWKVWDVS');`,
-          }}
-        />
+
         <Script
           src="https://t.contentsquare.net/uxa/43df6114dc031.js"
           strategy="beforeInteractive"
         />
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WWKVWDVS"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <PostHogProvider>
           <Providers>
             <QueryClientProviderWrapper>
